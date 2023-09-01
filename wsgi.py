950221ff5226f4d57943b894788a6311f4283663
Michaelangelo <michaelangeloifeanyichukwu@gmail.com>
2023-09-01 10:18:20 +0100

    first commit

--- wsgi.py
+++
@@ -1,6 +0,0 @@
-from app import app  # Import your Flask app instance
-from waitress import serve
-
-if __name__ == '__main__':
-    # Serve the app using Waitress
-    serve(app, host='0.0.0.0', port=8080)
